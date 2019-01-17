4279138aa4df509aa46e7b0df7af399eb706bc3f
unknown <30451868+ldy2119@users.noreply.github.com>
2019-01-17 23:54:27 +0900

    Update test14.py

--- Python/test14.py
+++ Python/test14.py
@@ -60,12 +60,21 @@ def Findaround(maskMap, x, y):
 
 def ccw(x1, y1, x2, y2, x3, y3):
     temp = x1 * y2 + x2 * y3 + x3 * y1
+    temp = temp - y1 * x2 - y2 * x3 - y3 * x1
     if temp > 0:
         return 1
     elif temp < 0:
         return -1
     else:
         return 0
+
+def isIntersect(x, y, x2, y2, x3, y3, x4, y4):
+    ab = ccw(x, y, x2, y2, x3, y3) * ccw(x, y, x2, y2, x4, y4)
+    cd = ccw(x3, y3, x4, y4, x, y) * ccw(x3, y3, x4, y4, x2, y2)
+    # print(ab, cd)
+
+    return ab <= 0 and cd <= 0
+
 
 #경로 설정
 moveList = [(220, 100)]
@@ -83,21 +92,51 @@ image, contours, hierachy = cv2.findContours(mask_, cv2.RETR_TREE,cv2.CHAIN_APPR
 image = img
 
 cnt = contours[6]
+cv2.circle(img, (220, 170), 3, (255, 5, 55), -1)
+cv2.circle(img, (260, 160), 3, (255, 5, 55), -1)
 
 hull = cv2.convexHull(cnt)
 for idx, arr in enumerate(hull):
     # print(hull[idx % len(hull)][0])
     x, y = hull[idx][0]
     x2, y2 = hull[(idx + 1) % len(hull)][0]
-    cv2.line(img, (x, y), (x2, y2), (255, 0, 0), 1)
-    print(x, y, x2, y2)
+    # cv2.line(img, (x, y), (x2, y2), (255, 0, 0), 1)
+    cv2.circle(img, (x, y), 3, (255, 5, 55), -1)
+    cv2.imshow("e", img)
+    ccwtmp = ccw(220, 170, x, y, 260, 160)
+    if ccwtmp >= 1:
+        
+    cv2.waitKey(0)
+    # print(isIntersect(220, 170, 260, 160, x, y, x2, y2))
+
+    # cv2.waitKey(0)
+    # cv2.imshow("e", image)
+    # print(x, y, x2, y2)
+
+# cv2.circle(img ,(x, y), 1, (255, 0, 0), -1)
+# cv2.line(img, (hull[0]), hull[int(len(hull)/2)], (255, 0, 0), 1)
 # cv2.line(img, (296, 164), (281, 174), (255, 0, 0), 1)
-cv2.circle(img, (260, 160), 3, (255, 5, 55), -1)
-cv2.circle(img, (230, 154), 3, (255, 5, 55), -1)
-cv2.circle(img, (220, 170), 3, (255, 5, 55), -1)
+# cv2.circle(img, (260, 160), 3, (255, 5, 55), -1)
+# cv2.circle(img, (230, 154), 3, (255, 5, 55), -1)
+# cv2.circle(img, (100, 100), 3, (255, 5, 55), -1)
+# cv2.waitKey(0)
+# cv2.imshow("e", image)
 
-print(ccw(260, 160, 230, 154, 220, 170))
+# cv2.circle(img, (60, 60), 3, (255, 5, 55), -1)
+# cv2.waitKey(0)
+# cv2.imshow("e", image)
 
+# cv2.circle(img, (70, 40), 3, (255, 5, 55), -1)
+# cv2.waitKey(0)
+# cv2.imshow("e", image)
+
+# cv2.circle(img, (260, 160), 1, (255, 5, 55), -1)
+# cv2.circle(img, (244, 167), 1, (255, 5, 55), -1)
+# cv2.circle(img, (228, 156), 1, (255, 5, 55), -1)
+# cv2.line(img, (244, 167), (228, 156), (255, 0, 0), 1)
+# cv2.line(img, (220, 170), (260, 160), (255, 0, 0), 1)
+
+# print(isIntersect(220, 170, 260, 160, 244, 167, 228, 156))
 
 # print(hull)
 # defects = cv2.convexityDefects(cnt, hull)
